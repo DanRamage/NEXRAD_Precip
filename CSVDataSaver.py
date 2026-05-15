d45d0da3a9ad4811d7ae8eaa076a1288fc211a5d
DANRAMAGE <ChiefDan@gmail.com>
2026-05-15 09:37:33 -0400

    Converted from Poetry to uv.

--- CSVDataSaver.py
+++ CSVDataSaver.py
@@ -8,25 +8,30 @@ from datetime import datetime
 from pandas import read_csv
 from pathlib import Path
 
+
 class nexrad_csv_saver(precipitation_saver):
-    def __init__(self, output_directory: Path,
-                 temp_directory: Path,
-                 source_tz: str,
-                 destination_tz: str):
+    def __init__(
+        self,
+        output_directory: Path,
+        temp_directory: Path,
+        source_tz: str,
+        destination_tz: str,
+    ):
         self._logger = logging.getLogger()
 
         self._new_records_added = 0
         self._boundary_output_files = {}
         self._now_date_time = datetime.now()
         self._finalized_filenames = []
-        #This is where we process the files before moving them to the final destination. This can be
-        #an NFS so we want to do all processing locally first to avoid any file corruption.
+        # This is where we process the files before moving them to the final destination. This can be
+        # an NFS so we want to do all processing locally first to avoid any file corruption.
         self._temp_directory = temp_directory
-        #The directory where the finalized file will be stored.
+        # The directory where the finalized file will be stored.
         self._output_directory = output_directory
         self._src_tz = pytz.timezone(source_tz)
         self._dst_tz = pytz.timezone(destination_tz)
         self._precip_files = {}
+
     @property
     def new_records_added(self):
         return self._new_records_added
@@ -36,15 +41,17 @@ class nexrad_csv_saver(precipitation_saver):
         return self._finalized_filenames
 
     def save(self, xmrg_results_data: xmrg_results):
-        '''
+        """
         Saves the xmrg_results_data to the CSV output file.
         :param xmrg_results_data:
         :return:
-        '''
+        """
         for boundary_name, boundary_results in xmrg_results_data.get_boundary_data():
             if boundary_name not in self._precip_files:
-                cleaned_boundary_name = boundary_name.replace(' ', '_')
-                file_path = self._temp_directory / f"{cleaned_boundary_name}_unsorted.csv"
+                cleaned_boundary_name = boundary_name.replace(" ", "_")
+                file_path = (
+                    self._temp_directory / f"{cleaned_boundary_name}_unsorted.csv"
+                )
                 write_header = False
                 if not file_path.exists():
                     write_header = True
@@ -56,13 +63,17 @@ class nexrad_csv_saver(precipitation_saver):
             precip_file_obj = self._precip_files[boundary_name]
             try:
 
-                avg = boundary_results['weighted_average']
-                #outstring = "%s,%s,%s,%f\n" % (boundary_name,xmrg_results_data.datetime,xmrg_results_data.datetime,avg)
-                #self._output_file_obj.write(outstring)
-                utc_datetime = self._src_tz.localize(datetime.strptime(xmrg_results_data.datetime, "%Y-%m-%dT%H:%M:%S"))
-                #local_datetime = utc_datetime.astimezone(self._dst_tz).strftime("%Y-%m-%dT%H:%M:%S")
-                precip_file_obj.write(f"{boundary_name},{utc_datetime},{utc_datetime},{avg:0.6f}\n")
-                #self._output_file_obj.write(f"{boundary_name},{local_datetime},{local_datetime},{avg:0.6f}\n")
+                avg = boundary_results["weighted_average"]
+                # outstring = "%s,%s,%s,%f\n" % (boundary_name,xmrg_results_data.datetime,xmrg_results_data.datetime,avg)
+                # self._output_file_obj.write(outstring)
+                utc_datetime = self._src_tz.localize(
+                    datetime.strptime(xmrg_results_data.datetime, "%Y-%m-%dT%H:%M:%S")
+                )
+                # local_datetime = utc_datetime.astimezone(self._dst_tz).strftime("%Y-%m-%dT%H:%M:%S")
+                precip_file_obj.write(
+                    f"{boundary_name},{utc_datetime},{utc_datetime},{avg:0.6f}\n"
+                )
+                # self._output_file_obj.write(f"{boundary_name},{local_datetime},{local_datetime},{avg:0.6f}\n")
             except Exception as e:
                 self._logger.exception(e)
 
@@ -71,7 +82,7 @@ class nexrad_csv_saver(precipitation_saver):
         This function is for us to clean up before the script exits.
         :return:
         """
-        #Close the open files.
+        # Close the open files.
         for boundary_name, precip_file_obj in self._precip_files.items():
             precip_file_obj.close()
         try:
@@ -79,43 +90,45 @@ class nexrad_csv_saver(precipitation_saver):
                 unsorted_filename = precip_file_obj.name
                 directory, filename = os.path.split(unsorted_filename)
                 filename = filename.replace("_unsorted.csv", ".csv")
-                self._logger.info(f"Sorting file file: {unsorted_filename} into file: {filename}")
+                self._logger.info(
+                    f"Sorting file file: {unsorted_filename} into file: {filename}"
+                )
                 final_filename = Path(directory) / filename
 
-                unsorted_pd_df = read_csv(unsorted_filename,
-                                 dtype={
-                                     "Area": str,
-                                     "Start Time": str,
-                                     "End Time": str,
-                                     "Weighted Average": str
-                                 },
-                                 parse_dates=["Start Time", "End Time"],
-                                 keep_default_na=False
-                                 )
+                unsorted_pd_df = read_csv(
+                    unsorted_filename,
+                    dtype={
+                        "Area": str,
+                        "Start Time": str,
+                        "End Time": str,
+                        "Weighted Average": str,
+                    },
+                    parse_dates=["Start Time", "End Time"],
+                    keep_default_na=False,
+                )
                 if final_filename.exists():
-                    pd_df = read_csv(final_filename,
-                                     dtype={
-                                         "Area": str,
-                                         "Start Time": str,
-                                         "End Time": str,
-                                         "Weighted Average": str
-                                     },
-                                     parse_dates=["Start Time", "End Time"],
-                                     keep_default_na=False
-                                     )
+                    pd_df = read_csv(
+                        final_filename,
+                        dtype={
+                            "Area": str,
+                            "Start Time": str,
+                            "End Time": str,
+                            "Weighted Average": str,
+                        },
+                        parse_dates=["Start Time", "End Time"],
+                        keep_default_na=False,
+                    )
                     result = (
-                        pd_df
-                        .set_index("Start Time")
+                        pd_df.set_index("Start Time")
                         .combine_first(unsorted_pd_df.set_index("Start Time"))
                         .reset_index()
                     )
-                    sorted_df = result.sort_values(by='Start Time')
+                    sorted_df = result.sort_values(by="Start Time")
                 else:
-                    sorted_df = unsorted_pd_df.sort_values(by='Start Time')
+                    sorted_df = unsorted_pd_df.sort_values(by="Start Time")
                 sorted_df.to_csv(final_filename, index=False)
                 destination_filename = self._output_directory / filename
                 self._logger.info(f"Deleting temp file: {unsorted_filename}")
                 final_filename.replace(destination_filename)
         except Exception as e:
             self._logger.exception(e)
-
--- main.py
+++ main.py
@@ -19,13 +19,15 @@ from xmrgprocessing.xmrgfileiterator.xmrg_file_iterator import xmrg_file_iterato
 from xmrgprocessing.xmrg_utilities import build_filename
 
 from CSVDataSaver import nexrad_csv_saver
+
+
 def load_boundaries_from_geojson(geojson_directory):
     logger = logging.getLogger()
-    watershed_list =  glob.glob(os.path.join(geojson_directory, "*.json"))
+    watershed_list = glob.glob(os.path.join(geojson_directory, "*.json"))
     boundaries = boundariesparse.Boundary(unique_id=uuid.uuid4())
     boundaries.parse_boundaries_file(geojson_directory)
     return boundaries
-    '''
+    """
     boundaries_tuples = []
     for watershed in watershed_list:
         try:
@@ -44,26 +46,27 @@ def load_boundaries_from_geojson(geojson_directory):
         #    polygon = geo_json['features'][0]['geometry']
         #    boundaries.append((name, polygon))
     return boundaries_tuples
-    '''
+    """
 
-def build_missing_date_list(precipitation_output_directory: Path, start_date: datetime, end_date: datetime):
+
+def build_missing_date_list(
+    precipitation_output_directory: Path, start_date: datetime, end_date: datetime
+):
     file_list = precipitation_output_directory.glob("*.csv")
     missing_data_list = []
     # Create the complete list of expected hourly datetimes
-    expected_times = pd.date_range(
-        start=start_date,
-        end=end_date,
-        freq="h"
-    )
+    expected_times = pd.date_range(start=start_date, end=end_date, freq="h")
     for file in file_list:
         csv_df = pd.read_csv(file, parse_dates=["Start Time", "End Time"])
         # Find missing hourly datetimes
         missing_times = expected_times.difference(csv_df["Start Time"])
-        #If we have existing missing date times, let's see if the latest missing_times has an entry we don't already have.
+        # If we have existing missing date times, let's see if the latest missing_times has an entry we don't already have.
         if len(missing_data_list):
             new_missing_times = missing_times.difference(missing_data_list)
             if len(new_missing_times):
-                missing_data_list.extend(pd.to_datetime(new_missing_times).to_pydatetime().tolist())
+                missing_data_list.extend(
+                    pd.to_datetime(new_missing_times).to_pydatetime().tolist()
+                )
         missing_times_list = pd.to_datetime(missing_times).to_pydatetime().tolist()
         if len(missing_data_list) == 0:
             missing_data_list = [date_time for date_time in missing_times_list]
@@ -71,32 +74,55 @@ def build_missing_date_list(precipitation_output_directory: Path, start_date: da
     missing_data_list.sort()
     return missing_data_list
 
+
 def main():
     parser = optparse.OptionParser()
 
+    parser.add_option(
+        "--LastNHours",
+        dest="last_n_hours",
+        help="Number of hours of XMRG files to download.",
+        default=None,
+        type="int",
+    )
 
-    parser.add_option("--LastNHours", dest="last_n_hours",
-                      help="Number of hours of XMRG files to download.", default=None,
-                      type="int")
+    parser.add_option(
+        "--StartDate",
+        dest="start_date",
+        help="The starting date to process XMRG files.",
+        default=(datetime.now() - timedelta(hours=72)).strftime("%Y-%m-%d %H:00:00"),
+    )
 
-    parser.add_option("--StartDate", dest="start_date",
-                      help="The starting date to process XMRG files.",
-                      default=(datetime.now() - timedelta(hours=72)).strftime("%Y-%m-%d %H:00:00"))
+    parser.add_option(
+        "--EndDate",
+        dest="end_date",
+        help="The ending date to process XMRG files.",
+        default=datetime.now().strftime("%Y-%m-%d %H:00:00"),
+    )
 
-    parser.add_option("--EndDate", dest="end_date",
-                      help="The ending date to process XMRG files.",
-                      default=datetime.now().strftime("%Y-%m-%d %H:00:00"))
+    parser.add_option(
+        "--DownloadDirectory",
+        dest="download_directory",
+        help="Directory to download XMRG files to.",
+        default=None,
+    )
 
-    parser.add_option("--DownloadDirectory", dest="download_directory",
-                      help="Directory to download XMRG files to.", default=None)
+    parser.add_option(
+        "--ConfigurationFile",
+        dest="config_file",
+        help="Configuration Settings.",
+        default=None,
+    )
 
-    parser.add_option("--ConfigurationFile", dest="config_file",
-                      help="Configuration Settings.", default=None)
+    parser.add_option(
+        "--FillGaps",
+        dest="fill_gaps",
+        action="store_true",
+        default=False,
+        help="Configuration Settings.",
+    )
 
-    parser.add_option("--FillGaps", dest="fill_gaps", action="store_true", default=False,
-                      help="Configuration Settings.")
-
-    (options, args) = parser.parse_args()
+    options, args = parser.parse_args()
 
     now_time = datetime.now()
     config_file = configparser.ConfigParser()
@@ -118,7 +144,9 @@ def main():
     url = config_file.get("xmrg", "url")
     save_all_precip_values = config_file.get("xmrg", "save_all_precip_values")
     delete_source_file = config_file.get("xmrg", "delete_source_file")
-    delete_compressed_source_file = config_file.get("xmrg", "delete_compressed_source_file")
+    delete_compressed_source_file = config_file.get(
+        "xmrg", "delete_compressed_source_file"
+    )
     download_directory = config_file.get("xmrg", "download_directory")
     xmrg_base_directory = config_file.get("xmrg", "base_xmrg_directory")
     kml_output_directory = config_file.get("xmrg", "kml_output_directory")
@@ -126,27 +154,35 @@ def main():
     boundary_directory = config_file.get("watershed", "directory")
     sqlite_file = config_file.get("xmrg", "database_file")
     worker_process_count = config_file.getint("xmrg", "worker_process_count")
-    precipitation_temp_output_directory = Path(config_file.get("xmrg", "precipitation_temp_output_directory"))
+    precipitation_temp_output_directory = Path(
+        config_file.get("xmrg", "precipitation_temp_output_directory")
+    )
     precipitation_temp_output_directory.mkdir(exist_ok=True)
-    precipitation_output_directory = Path(config_file.get("xmrg", "precipitation_output_directory"))
+    precipitation_output_directory = Path(
+        config_file.get("xmrg", "precipitation_output_directory")
+    )
     precipitation_output_directory.mkdir(exist_ok=True)
-    #data_saver = nexrad_xenia_sqlite_saver(sqlite_file)
-    data_saver = nexrad_csv_saver(precipitation_output_directory,
-                                  precipitation_temp_output_directory,
-                                  "UTC",
-                                  "US/Eastern")
+    # data_saver = nexrad_xenia_sqlite_saver(sqlite_file)
+    data_saver = nexrad_csv_saver(
+        precipitation_output_directory,
+        precipitation_temp_output_directory,
+        "UTC",
+        "US/Eastern",
+    )
 
     boundaries = load_boundaries_from_geojson(boundary_directory)
 
-    wkt_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "boundaries.csv")
+    wkt_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "boundaries.csv"
+    )
     with open(wkt_file, "w") as wkt_obj:
         for boundary in boundaries.boundaries:
             boundary_polygon = shape(boundary[1])
-            wkt_obj.write(f"\"{boundary[0]}\",\"{boundary_polygon.wkt}\"\n")
+            wkt_obj.write(f'"{boundary[0]}","{boundary_polygon.wkt}"\n')
 
-    #We operate in EST, nexrad files are in UTC.
-    utcTZ = timezone('UTC')
-    estTZ = timezone('US/Eastern')
+    # We operate in EST, nexrad files are in UTC.
+    utcTZ = timezone("UTC")
+    estTZ = timezone("US/Eastern")
 
     start_date = options.start_date
     if type(start_date) != datetime:
@@ -162,16 +198,21 @@ def main():
     utc_end_date = end_date.astimezone(utcTZ)
     utc_start_date = start_date.astimezone(utcTZ)
 
-    missing_dates = build_missing_date_list(precipitation_temp_output_directory, utc_start_date, utc_end_date)
+    missing_dates = build_missing_date_list(
+        precipitation_temp_output_directory, utc_start_date, utc_end_date
+    )
 
     task_id = uuid4()
     if len(missing_dates):
-        xmrg_iterator = xmrg_file_iterator(date_list=missing_dates,
-                                           base_xmrg_path=xmrg_base_directory)
+        xmrg_iterator = xmrg_file_iterator(
+            date_list=missing_dates, base_xmrg_path=xmrg_base_directory
+        )
     else:
-        xmrg_iterator = xmrg_file_iterator(start_date=utc_start_date,
-                                           end_date=utc_end_date,
-                                           base_xmrg_path=xmrg_base_directory)
+        xmrg_iterator = xmrg_file_iterator(
+            start_date=utc_start_date,
+            end_date=utc_end_date,
+            base_xmrg_path=xmrg_base_directory,
+        )
 
     xmrg_proc = xmrg_process(
         file_list_iterator=xmrg_iterator,
@@ -186,13 +227,18 @@ def main():
         kml_output_directory=kml_output_directory,
         save_all_precip_values=save_all_precip_values,
         delete_source_file=True,
-        delete_compressed_source_file=True)
-    xmrg_proc.process(start_date=utc_start_date, end_date=utc_end_date,
-                      base_xmrg_directory=xmrg_base_directory)
+        delete_compressed_source_file=True,
+    )
+    xmrg_proc.process(
+        start_date=utc_start_date,
+        end_date=utc_end_date,
+        base_xmrg_directory=xmrg_base_directory,
+    )
 
     logger.info("Logging stopped.")
 
     return
+
 
 if __name__ == "__main__":
     main()
